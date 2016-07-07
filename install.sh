a011f75aa8f4005287df5810c90d6f942a5b89c5
Kern Handa <kern.handa@gmail.com>
2016-07-07 20:51:35 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+sudo apt-get install byobu
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 ln -s $SCRIPTPATH/vimconf/.vimrc ~/.vimrc
 ln -s $SCRIPTPATH/.vimrc.bundles ~/.vimrc.bundles
 ln -s $SCRIPTPATH/.vimrc.last ~/.vimrc.last
+ln -s $SCRIPTPATH/.bash_profile ~/.bash_profile
+ln -s $SCRIPTPATH/.bashrc ~/.bashrc
+ln -s $SCRIPTPATH/.spacemacs ~/.spacemacs
 
