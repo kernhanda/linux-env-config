3c63eea2270ae03ee76cc93e0201619b1158b155
Kern Handa <kern.handa@gmail.com>
2016-07-26 22:06:03 +0000

    Change shell to zsh, install oh-my-zsh

--- install.sh
+++ install.sh
@@ -13,4 +13,6 @@ ln -f -s $SCRIPTPATH/.bashrc ~/.bashrc
 ln -f -s $SCRIPTPATH/.spacemacs ~/.spacemacs
 ln -f -s $SCRIPTPATH/.zshrc ~/.zshrc
 
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
