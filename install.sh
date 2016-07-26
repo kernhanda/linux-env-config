63ce64a51cc6f7b2d4e8ff96d01b6e52bff86442
Kern Handa <kern.handa@gmail.com>
2016-07-26 22:08:33 +0000

    Change ordering

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
+
+chsh -s $(which zsh)
+mkdir ~/.oh-my-zsh && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 ln -f -s $SCRIPTPATH/vimconf/.vimrc ~/.vimrc
 ln -f -s $SCRIPTPATH/.vimrc.bundles ~/.vimrc.bundles
 ln -f -s $SCRIPTPATH/.vimrc.last ~/.vimrc.last
@@ -13,6 +17,4 @@ ln -f -s $SCRIPTPATH/.bashrc ~/.bashrc
 ln -f -s $SCRIPTPATH/.spacemacs ~/.spacemacs
 ln -f -s $SCRIPTPATH/.zshrc ~/.zshrc
 
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
