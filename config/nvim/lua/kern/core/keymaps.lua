2c98a1d468963b98a96d184c36bd0d2971db42b2
Kern Handa <kern.handa@gmail.com>
2024-05-26 14:52:39 -0700

    Add hardtime and precognition nvim plugins + keybindings

--- config/nvim/lua/kern/core/keymaps.lua
+++ config/nvim/lua/kern/core/keymaps.lua
@@ -5,14 +5,12 @@ local keymap = vim.keymap -- convenience
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
-keymap.set("n", "x", '"_x') -- deletes single character without copying into register
-
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
-keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" })
-keymap.set("n", "<leader>wc", "<cmd>close<CR>", { desc = "Close current split" })
+keymap.set("n", "<leader>n", "<cmd>bn<cr>", { desc = "Next buffer" })
+keymap.set("n", "<leader>p", "<cmd>bp<cr>", { desc = "Prev buffer" })
+keymap.set("n", "<leader>ww", "<cmd>set wrap!<cr>", { desc = "Toggle word wrap" })
 
 keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close current tab" })
@@ -20,4 +18,4 @@ keymap.set("n", "<leader>ty", "<cmd>tabn<CR>", { desc = "Next tab" })
 keymap.set("n", "<leader>tr", "<cmd>tabp<CR>", { desc = "Prev tab" })
 keymap.set("n", "<leader>td", "<cmd>tabnew %<CR>", { desc = "Dupe current buffer in new tab" })
 
-
+keymap.set("n", "<leader>!!", "<cmd>Hardtime toggle<cr>", { desc = "Toggle hardtime" })
