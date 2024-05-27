5c267b7c78c9da08f906f6ef52e2db6ecb57796b
Kern Handa <kern.handa@gmail.com>
2024-05-26 21:01:25 -0700

    Update telescope path display

--- config/nvim/lua/kern/plugins/telescope.lua
+++ config/nvim/lua/kern/plugins/telescope.lua
@@ -16,7 +16,7 @@ return {
 
 		telescope.setup({
 			defaults = {
-				path_display = { "smart" },
+				path_display = { "filename_first" },
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
