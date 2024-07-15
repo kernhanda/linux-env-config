1714300f39c11e4b50089e321562dcbf57459010
Kern Handa <kern.handa@gmail.com>
2024-07-15 15:33:28 -0700

    Change nvim-comment keymap

--- config/nvim/lua/kern/plugins/comment.lua
+++ config/nvim/lua/kern/plugins/comment.lua
@@ -4,8 +4,7 @@ return {
     name = "nvim_comment",
     config = true,
     keys = {
-        { "<C-/>", "<CMD>CommentToggle<CR>j", mode = { "n" } },
-        { "<C-/>", "<C-\\><C-N><CMD>CommentToggle<CR>ji", mode = { "i" } },
-        { "<C-/>", ":'<,'>CommentToggle<CR>gv<esc>j", mode = { "v" } },
+        { "<leader>cc", "<CMD>CommentToggle<CR>j", mode = { "n" } },
+        { "<leader>cc", ":'<,'>CommentToggle<CR>gv<esc>j", mode = { "v" } },
     },
 }
