91eadd324799273691340a9cdbe28e6be3eecce2
Kern <kern@modular.com>
2025-02-14 20:22:23 +0000

    Reduce zen mode width

--- config/nvim/lua/kern/plugins/zen-mode.lua
+++ config/nvim/lua/kern/plugins/zen-mode.lua
@@ -2,7 +2,7 @@ return {
     "folke/zen-mode.nvim",
     opts = {
         window = {
-            width = 0.6,
+            width = 0.3,
             options = {
                 number = true,
                 relativenumber = true,
