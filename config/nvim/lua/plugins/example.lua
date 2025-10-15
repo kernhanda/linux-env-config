7201c650c455551b7fa570a874cd218f8b378b75
Kern Handa <kern.handa@gmail.com>
2025-10-15 15:47:47 -0700

    Updates

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -30,6 +30,9 @@ return {
             },
           },
         },
+        mojo = {
+                cmd = {"mojo-lsp-server", "-I", "open-source/max/mojo/stdlib/stdlib"},
+        }
       },
       setup = {
         clangd = function(_, opts)
@@ -37,6 +40,7 @@ return {
           local lspconfig = require("lspconfig")
           lspconfig.clangd.setup(opts)
         end,
+        mojo = function(_, opts) end,
       },
     },
   },
@@ -64,4 +68,29 @@ return {
       },
     },
   },
+
+  -- Mason DAP
+  {
+  "jay-babu/mason-nvim-dap.nvim",
+  dependencies = "mason.nvim",
+  cmd = { "DapInstall", "DapUninstall" },
+  opts = {
+    -- Makes a best effort to setup the various debuggers with
+    -- reasonable debug configurations
+    automatic_installation = true,
+
+    -- You can provide additional configuration to the handlers,
+    -- see mason-nvim-dap README for more information
+    handlers = {},
+
+    -- You'll need to check that you have the required things installed
+    -- online, please don't ask me how to install them :)
+    ensure_installed = {
+      -- Update this to ensure that you have the debuggers for the langs you want
+                "codelldb",
+    },
+  },
+  -- mason-nvim-dap is loaded when nvim-dap loads
+  config = function() end,
+}
 }
