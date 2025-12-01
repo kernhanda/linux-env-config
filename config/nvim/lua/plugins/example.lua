ec5097e4cbe734ba94d6f862f40b7f0996cab9f5
Kern Handa <kern.handa@gmail.com>
2025-12-01 14:16:42 -0800

    [nvim] update flash colors

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -2,7 +2,40 @@
 -- stylua: ignore
 -- if true then return {} end
 -- ~/.config/nvim/lua/plugins/lsp.lua
+local function setupCustomHighlightGroup()
+	vim.api.nvim_command("hi clear FlashMatch")
+	vim.api.nvim_command("hi clear FlashCurrent")
+	vim.api.nvim_command("hi clear FlashLabel")
+    vim.api.nvim_command("hi clear FlashBackdrop")
+
+	vim.api.nvim_command("hi FlashMatch guibg=#3b4261 guifg=#7aa2f7 gui=bold")
+	vim.api.nvim_command("hi FlashCurrent guibg=#9ece6a guifg=#1a1b26 gui=bold")
+	vim.api.nvim_command("hi FlashLabel guibg=#ff9e64 guifg=#1a1b26 gui=bold")
+    vim.api.nvim_command("hi FlashBackdrop guibg=NONE guifg=#545c7e")
+end
+
 return {
+  {
+    "folke/flash.nvim",
+    opts = {
+      rainbow = {
+        enabled = true,
+        shade = 5,
+      },
+      highlight = {
+        backdrop = true,
+        groups = {
+          match = "FlashMatch",
+          current = "FlashCurrent",
+          backdrop = "FlashBackdrop",
+          label = "FlashLabel",
+        },
+      },
+    },
+    config = function()
+      setupCustomHighlightGroup()
+    end,
+  },
   -- LSP Configuration
   {
     "neovim/nvim-lspconfig",
@@ -31,8 +64,8 @@ return {
           },
         },
         mojo = {
-                cmd = {"mojo-lsp-server", "-I", "open-source/max/mojo/stdlib/stdlib"},
-        }
+          cmd = { "mojo-lsp-server", "-I", "open-source/max/mojo/stdlib/stdlib" },
+        },
       },
       setup = {
         clangd = function(_, opts)
@@ -71,26 +104,34 @@ return {
 
   -- Mason DAP
   {
-  "jay-babu/mason-nvim-dap.nvim",
-  dependencies = "mason.nvim",
-  cmd = { "DapInstall", "DapUninstall" },
-  opts = {
-    -- Makes a best effort to setup the various debuggers with
-    -- reasonable debug configurations
-    automatic_installation = true,
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = "mason.nvim",
+    cmd = { "DapInstall", "DapUninstall" },
+    opts = {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
 
-    -- You can provide additional configuration to the handlers,
-    -- see mason-nvim-dap README for more information
-    handlers = {},
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
 
-    -- You'll need to check that you have the required things installed
-    -- online, please don't ask me how to install them :)
-    ensure_installed = {
-      -- Update this to ensure that you have the debuggers for the langs you want
-                "codelldb",
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
+        "codelldb",
+      },
     },
+    -- mason-nvim-dap is loaded when nvim-dap loads
+    config = function() end,
   },
-  -- mason-nvim-dap is loaded when nvim-dap loads
-  config = function() end,
-}
+  {
+    "xTacobaco/cursor-agent.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>zz", ":CursorAgent<CR>", { desc = "Cursor Agent: Toggle terminal" })
+      vim.keymap.set("v", "<leader>zz", ":CursorAgentSelection<CR>", { desc = "Cursor Agent: Send selection" })
+      vim.keymap.set("n", "<leader>zZ", ":CursorAgentBuffer<CR>", { desc = "Cursor Agent: Send buffer" })
+    end,
+  },
 }
